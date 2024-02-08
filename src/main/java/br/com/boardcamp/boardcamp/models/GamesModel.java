0dc6eb1e8e12ad295448cee9e70d2a544477eb94
Jean Frusca <jeang96@gmail.com>
2024-02-08 11:22:43 -0300

    [feature] creation of games model

---
+++ src/main/java/br/com/boardcamp/boardcamp/models/GamesModel.java
@@ -0,0 +1,34 @@
+package br.com.boardcamp.boardcamp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "games")
+public class GamesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private Integer stockTotal;
+
+    @Column(nullable = false)
+    private Integer pricePerDay;
+}
