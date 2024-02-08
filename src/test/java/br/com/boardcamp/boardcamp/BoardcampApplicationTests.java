82e4b9b8ecf5ccdf3a51590572cddf844a4ac5fe
Jean Frusca <jeang96@gmail.com>
2024-02-08 10:47:16 -0300

    initial commit

---
+++ src/test/java/br/com/boardcamp/boardcamp/BoardcampApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boardcamp.boardcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
